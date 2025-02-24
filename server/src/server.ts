ad1a6a776cd864e593483736f1599a75e64f709d
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-02-24 16:06:58 -0300

    fix: resolvendo conflitos de merge

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,23 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './config/sequelize';
 import { connectMongoDB } from './config/database';
+import router from './routes/router';
 import HttpError from './utils/error/httpError';
-import cors from 'cors';
-import router, { configIndeceServerPoint } from './routes/router';
 import errorMiddleware from './utils/middlewares/errorMiddleware';
 
 dotenv.config();
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3001
 const server = express();
-
-export const indeceServerPoint = `/api`;
-configIndeceServerPoint(indeceServerPoint);
-
-server.use(cors())
 server.use(express.json());
-server.use(`${indeceServerPoint}`, router);
+server.use('/api', router);
 server.use(errorMiddleware);
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}\n`);
+})
 
 sequelize.sync().then(() => {
     console.log("Database connected successfully");
