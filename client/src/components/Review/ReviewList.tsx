a627a54f2269ab7afd57cc41d5be61053cda812d
luizfernandoin <luizfernandoinfo100@gmail.com>
2025-02-25 10:40:06 -0300

    feat: componente para listar reviews

---
+++ client/src/components/Review/ReviewList.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { getReviews } from '../../service/reviewService';
+import { IUser, getUserById } from '../../service/userService';
+
+
+interface IReview {
+    _id: string;
+    comment: string;
+    rating: number;
+    userID: string;
+    locationId: string;
+}
+
+const ReviewList: React.FC<{ locationId: string }> = ({ locationId }) => {
+    const [reviews, setReviews] = useState<IReview[]>([]);
+    const [users, setUsers] = useState<{ [key: string]: IUser }>({});
+
+    useEffect(() => {
+        const fetchReviews = async () => {
+            try {
+                const data = await getReviews(locationId);
+                setReviews(data);
+
+                const userIds = data.map((review: IReview) => review.userID);
+                const uniqueUserIds = [...new Set(userIds)];
+                const usersData: { [key: string]: IUser } = {};
+
+                for (const userId of uniqueUserIds) {
+                    if (typeof userId === 'string') {
+                        const user = await getUserById(userId);
+                        if (user) {
+                            usersData[userId] = user;
+                        }
+                    }
+                }
+                
+                setUsers(usersData);
+            } catch (error) {
+                if (error instanceof Error) {
+                    console.log(error.message);
+                }
+
+                console.log("Erro ao buscar reviews");
+            }
+        };
+
+        fetchReviews();
+    }, [locationId]);
+
+    return (
+        <div>
+            <h3>Avaliações</h3>
+            {reviews.length > 0 ? (
+                <ul>
+                    {reviews.map((review) => (
+                        <li key={review._id}>
+                            <strong>{users[review.userID]?.name || 'Usuário desconhecido'}</strong>: {review.comment} ({review.rating}/5)
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Sem avaliações ainda</p>
+            )}
+        </div>
+    );
+};
+
+export default ReviewList;
